13eac3d6c9cd3935629d662074c7397657e4deb3
Lee Hyunwoo <yong8947@hanyang.ac.kr>
2024-12-04 20:53:39 +0900

    test

--- dev/OneHotEncode/OneHot.py
+++ dev/OneHotEncode/OneHot.py
@@ -1,12 +1,19 @@
-def one_hot_encode(lables, num_classes):
+def one_hot_encode(labels, num_classes):
     
-    ont_hot = [] # 결과값 저장
+    one_hot = [] # 결과값 저장
 
     for label in labels:
 
         vector = [0] *  num_classes 
         vector[label] = 1
-        ont_hot.append(vector) # 백터로 된 결과값 리스트에 저장
+        one_hot.append(vector) # 백터로 된 결과값 리스트에 저장
 
-    return one_hot 
+    return one_hot
+
+
+#test
+a = [0, 1, 2 ,3, 5, 8]
+print(one_hot_encode(a, 9))
+
+
 
