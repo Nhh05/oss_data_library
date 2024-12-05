16fa4dea70e5b3d21ddb6cff91e1763eb85a36a3
gitleejm <leejm8422@hanyang.ac.kr>
2024-12-05 22:45:17 +0900

    modify function

--- dev/Scaling/scaling_.py
+++ dev/Scaling/scaling_.py
@@ -1,11 +1,17 @@
-data = []
+def scale_data(data): #매서드화
+    
+    data_min = min(data) #최솟값
+    data_max = max(data) #최댓값
+   
+    if data_min == data_max:  #최대와 최소가 같을 경우 예외 처리
+        return[1/data_min] * len(data)
+    
+    else:
+        scaled_data = []     #스케일링 된 수를 넣는 리스트
 
-data_min = min(data)
-data_max = max(data)
-scaled_data = []
+        for k in data:       #데이터를 0과 1사이로 스케일링 하는 코드
+            scaled_value = (k - data_min) / (data_max - data_min) 
+            scaled_data.append(scaled_value) 
 
-for k in data:
-    scaled_value = (k - data_min) / (data_max - data_min)
-    scaled_data.append(scaled_value)
+        return sorted(scaled_data)
 
-print(sorted(scaled_data))
