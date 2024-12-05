c2a234fd86d6a4fd81845141cdcd5e3fef0faedd
Lee Hyunwoo <yong8947@hanyang.ac.kr>
2024-12-05 16:22:07 +0900

    Class updated

--- dev/OneHotEncode/OneHot.py
+++ dev/OneHotEncode/OneHot.py
@@ -1,4 +1,4 @@
-def one_hot_encode(labels, num_classes):
+"""/def one_hot_encode(labels, num_classes):
     
     one_hot = [] # 결과값 저장
 
@@ -13,7 +13,24 @@ def one_hot_encode(labels, num_classes):
 
 #test
 a = [0, 1, 2 ,3, 5, 8]
-print(one_hot_encode(a, 9))
+print(one_hot_encode(a, 9)) """
+
+# 객체로 생성
+class OneHotEncoder:
+    def encode(self, labels):
+        num_classes = max(labels) + 1 # 라벨을 입력하면 넘클래스 자동 생성
+        one_hot = []
+        for label in labels:
+            vector = [0] * num_classes
+            vector[label] = 1
+            one_hot.append(vector)
+        return one_hot
+
+
+# Test
+encoder = OneHotEncoder()
+labels = [0, 1, 2, 3, 5, 8]
+print(encoder.encode(labels))
 
 
 
