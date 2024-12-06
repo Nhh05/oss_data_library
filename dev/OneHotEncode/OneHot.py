bef2d8ee5ac88fbfca7215d112473c4edf59b746
Lee Hyunwoo <yong8947@hanyang.ac.kr>
2024-12-06 17:26:05 +0900

    fin

--- dev/OneHotEncode/OneHot.py
+++ dev/OneHotEncode/OneHot.py
@@ -17,20 +17,21 @@ print(one_hot_encode(a, 9)) """
 
 # 객체로 생성
 class OneHotEncoder:
+    
     def encode(self, labels):
         num_classes = max(labels) + 1 # 라벨을 입력하면 넘클래스 자동 생성
-        one_hot = []
-        for label in labels:
+        one_hot = [] # 결과값 저장
+        for label in labels: 
             vector = [0] * num_classes
             vector[label] = 1
-            one_hot.append(vector)
+            one_hot.append(vector) # 백터로 된 결과값 리스트에 저장
         return one_hot
 
 
 # Test
-encoder = OneHotEncoder()
+"""encoder = OneHotEncoder()
 labels = [0, 1, 2, 3, 5, 8]
-print(encoder.encode(labels))
+print(encoder.encode(labels))"""
 
 
 
