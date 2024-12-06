1892b386bc48dfc508f803550af4ff4530fa2cec
Hahyeon <namhahyeon@hanyang.ac.kr>
2024-12-06 18:56:41 +0900

    dt_sp_update

--- dev/DataSplit/data_split.py
+++ dev/DataSplit/data_split.py
@@ -1,28 +1,31 @@
-
 import random
 
-class FisherYatesShuffler2D:
-    def __init__(self, seed=None):
-        
+class DataPreprocessor:
+    """데이터 변환 및 전처리 클래스"""
+    def __init__(self):
+        self.seed = None
+
+    def shuffle_train_test_data(self, x_data, y_data, train_ratio=0.7, seed=None):
+       
+        # 랜덤 시드 설정
         self.seed = seed
         if seed is not None:
-            random.seed(seed)
+            random.seed(self.seed)
 
-    def shuffle_rows(self, data):
-        
-        random.shuffle(data)  # 행 순서 섞기
-        return data
+        # x_data와 y_data를 짝으로 묶음
+        combined_data = list(zip(x_data, y_data))
 
-    def set_seed(self, seed):
-        
-        self.seed = seed
-        random.seed(seed)
+        # 데이터 섞기
+        random.shuffle(combined_data)
 
-    def split_data(data, train_ratio=0.7):
-    
-        split_index = int(len(data) * train_ratio)
-        train_data = data[:split_index]
-        test_data = data[split_index:]
-        return train_data, test_data
+        # 다시 x와 y로 분리
+        x_data, y_data = zip(*combined_data)
 
- 
+        # 데이터 분리
+        split_index = int(len(x_data) * train_ratio)
+        train_x = x_data[:split_index]
+        train_y = y_data[:split_index]
+        test_x = x_data[split_index:]
+        test_y = y_data[split_index:]
+
+        return train_x, train_y, test_x, test_y
