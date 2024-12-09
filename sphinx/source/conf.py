624b737706ded54f566b4db6d2a819663a9fb5b3
Hahyeon <namhahyeon@hanyang.ac.kr>
2024-12-09 09:33:59 +0900

    Update conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -16,6 +16,8 @@ release = '0.1'
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
 
 templates_path = ['_templates']
@@ -26,7 +28,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 import os
